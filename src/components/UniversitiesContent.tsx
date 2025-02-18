a96877df14ba66d3edef70a3299874b3c8cbc04e
erinc8 <erinc8@bu.edu>
2025-02-18 14:38:06 -0500

    dfsd

--- src/components/UniversitiesContent.tsx
+++ src/components/UniversitiesContent.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { UniversityPreview } from "./UniversitiesPreview";
-import { University } from '../type';
 
 
 export default function UniversitiesContent() {
